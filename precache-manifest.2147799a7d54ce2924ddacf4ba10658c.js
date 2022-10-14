3dae82597fecc564724da798ec8efc93cbc24df2
jongwoo1126 <lucky4527@gmail.com>
2022-10-14 10:11:53 +0900

    Updates

---
+++ precache-manifest.2147799a7d54ce2924ddacf4ba10658c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "10cd5200d38207d550ac",
+    "url": "/Practice/static/js/main.04be70bd.chunk.js"
+  },
+  {
+    "revision": "9e3d73ddcc4de4d76d37",
+    "url": "/Practice/static/js/runtime~main.18cfb0de.js"
+  },
+  {
+    "revision": "89a34c04818a758186dd",
+    "url": "/Practice/static/js/2.5667b7f5.chunk.js"
+  },
+  {
+    "revision": "18fc7b3f6fcc9fe027200e86f98cfb1e",
+    "url": "/Practice/index.html"
+  }
+];
